5480e10b418083cba358325df6b9aea909c2525a
Rafijoee <rafijoe44@gmail.com>
2026-06-26 05:51:45 +0700

    update crud pengunjung

--- app/Http/Controllers/PengunjungController.php
+++ app/Http/Controllers/PengunjungController.php
@@ -94,7 +94,7 @@ public function create()
     {
         $validated = $request->validate([
             'nama'             => 'required|string|max:255',
-            'nik'              => 'required|string|max:20|unique:Pengunjungs,nik,' . $Pengunjung->id,
+            'nik'              => 'required|string|max:20|unique:Pengunjungs,nik,',
             'telepon'          => 'required|string|max:20',
             'deteni_id'        => 'required|exists:detenis,id',
             'jadwal_kunjungan' => 'required|date',
